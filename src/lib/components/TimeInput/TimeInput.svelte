6c0db28f60da583a47bb911375508d593ba556f9
Siddhesh Mangela <sidthecool007@gmail.com>
2026-08-15 16:48:57 +0530

    Update TimeInput.svelte

--- src/lib/components/TimeInput/TimeInput.svelte
+++ src/lib/components/TimeInput/TimeInput.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  import { InputEnclosure, type InputEnclosureProps } from '@flightlesslabs/dodo-ui';
+  import { InputEnclosure, UtilityButton, type InputEnclosureProps } from '@flightlesslabs/dodo-ui';
+  import Icon from '@iconify/svelte';
   import { TimeField, type WithoutChildrenOrChild } from 'bits-ui';
 
   type Props = {
     class?: string;
+    clearable?: boolean;
   };
 
   let {
@@ -15,12 +17,21 @@
     outline,
     disabled,
     error,
-    before,
-    after,
+    clearable,
     ...restProps
   }: Props & InputEnclosureProps & WithoutChildrenOrChild<TimeField.RootProps> = $props();
 
   const classes = $derived(['TimeInput', className].filter(Boolean));
+
+  let isFocused = $state(false);
+
+  function handleFocus() {
+    isFocused = true;
+  }
+
+  function handleBlur() {
+    isFocused = false;
+  }
 </script>
 
 <InputEnclosure
@@ -30,11 +41,11 @@
   {outline}
   {disabled}
   {error}
-  {before}
-  {after}
+  affixSpacingBefore="tight"
+  focused={isFocused}
 >
   <TimeField.Root bind:value {placeholder} {...restProps} hourCycle={12} {disabled}>
-    <TimeField.Input class="InputBox">
+    <TimeField.Input class="InputBox" onfocus={handleFocus} onblur={handleBlur}>
       {#snippet children({ segments })}
         {#each segments as { part, value }, i (i)}
           <TimeField.Segment {part}>
@@ -44,6 +55,26 @@
       {/snippet}
     </TimeField.Input>
   </TimeField.Root>
+
+  {#snippet before()}
+    <Icon icon="mingcute:time-line" />
+  {/snippet}
+
+  {#snippet after()}
+    {#if clearable && value}
+      <UtilityButton
+        class="ClearButton"
+        roundness="full"
+        {size}
+        compact
+        color="primary"
+        {disabled}
+        onclick={() => (value = undefined)}
+      >
+        <Icon icon="material-symbols:close" />
+      </UtilityButton>
+    {/if}
+  {/snippet}
 </InputEnclosure>
 
 <style lang="scss">
@@ -52,7 +83,27 @@
     display: flex;
   }
 
-  :global(.TimeInput [data-segment='minute']) {
-    margin-right: calc(var(--dodo-ui-space) / 2);
+  :global(.ClearButton) {
+    font-size: 1.25em;
+  }
+
+  :global(.TimeInput [data-segment]) {
+    align-items: center;
+    height: 80%;
+    display: inline-flex;
+    padding: 0 calc(var(--dodo-ui-space) / 3);
+  }
+
+  :global(.TimeInput [data-segment='literal']) {
+    padding: 0;
+  }
+
+  :global(.TimeInput .dodo-ui-InputAffix.placement--before) {
+    margin: 0 calc(var(--dodo-ui-space) / 2.5);
+    width: calc(var(--dodo-ui-element-height-normal) / 1.2);
+    font-size: 1.3em;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
